c32d099ed63804bc77e055caa31ba3b807022ab7
cyt <1793402316@qq.com>
2023-09-11 00:02:29 +0800

    ✨feat: 登录拦截器 session校验

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,38 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author cyt
+ * @version 1.0
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
+                // 排除不需要拦截路径
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+        // token刷新的拦截器
+//        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
+    }
+}
--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -3,9 +3,12 @@ package com.hmdp.controller;
 
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -64,10 +67,17 @@ public class UserController {
         return Result.fail("功能未完成");
     }
 
+    /**
+     * 拦截器校验 是否登录
+     *
+     * @return
+     */
     @GetMapping("/me")
     public Result me() {
         // TODO 获取当前登录的用户并返回
-        return Result.fail("功能未完成");
+        User user = UserHolder.getUser();
+        return Result.ok(user);
+//        return Result.fail("功能未完成");
     }
 
     @GetMapping("/info/{id}")
